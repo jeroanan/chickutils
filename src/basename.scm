d50f0d2b8d8d062a84f1ea936f72e2e3094d66ec
Dave Wilson <davidwil@posteo.de>
2022-03-28 21:50:37 +0100

    Add -a flag to basename

--- src/basename.scm
+++ src/basename.scm
@@ -1,5 +1,5 @@
 (import (chicken process-context)
-        (chicken string))
+        (chicken string) (chicken format))
 
 (declare (uses list-util))
 
@@ -7,13 +7,46 @@
   (if (< (length (command-line-arguments)) n)
       (error error-message)))
 
+(define (command-line-has-flag? flag)
+  (any? (lambda (a) (string=? flag a))
+        (command-line-arguments)))
+
+(define (get-command-line-without-flags flag-map)
+  (define (loop flags i)
+    (if (empty? flags) 
+        i
+        (let* ((flag-entry (first flags)) 
+               (the-flag (first flag-entry)) 
+               (flag-has-param (second flag-entry)))
+          (if (command-line-has-flag? the-flag)
+              (if (eq? flag-has-param #t)
+                  (loop (rest flags) (+ i 2))
+                  (loop (rest flags) (+ i 1)))
+              (loop (rest flags) i)))))
+  (drop (loop flag-map 0) (command-line-arguments)))
+
+(define flag-map (list 
+                    (list "-a" #f)
+                    (list "-s" #t)
+                    (list "-z" #f)))
+
 (enforce-minimum-commmand-line-args 1 "Missing operand")
 
-(define args (command-line-arguments))
-(define the-dir (first args))
-(define components (string-split the-dir "/" #t))
-(define output (if (string=? (last components) "") "/" (last components)))
+(define (get-basename name) 
+  (let ((components (string-split name "/" #t)))
+    (if (string=? (last components) "")
+        "/"
+        (last components))))
 
-(print output)
+(define (display-basenames names)
+  (define (loop names) 
+    (unless (empty? names) 
+      (begin 
+        (print (get-basename (first names)))
+        (loop (rest names)))))
+  (loop names))
 
-
+(define names (get-command-line-without-flags flag-map))
+(if (command-line-has-flag? "-a")
+    (display-basenames names)
+    (display-basenames (list (first names))))
--- src/util/list-util.scm
+++ src/util/list-util.scm
@@ -1,6 +1,8 @@
 (declare (unit list-util))
 
 (define first car)
+(define (second list) (take 1 (cdr list)))
+
 (define (last l)
   (first (reverse l)))
 
@@ -12,3 +14,17 @@
             res
             (loop (cdr lst) (flatten (list res (car lst))) (- n 1))))
     (loop lst (list) n))
+
+(define drop (lambda (n lst)
+    (if (= n 0)
+        lst
+        (drop (- n 1) (cdr lst)))))
+
+(define (empty? lst) (= (length lst) 0))
+
+(define (any? pred lst)
+    (define (loop lst)
+        (if (null? lst)
+            #f
+            (or (pred (car lst)) (loop (cdr lst)))))
+    (loop lst))
