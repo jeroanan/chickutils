fc2eb2e5ee96f725b612234b97ef3a1c8b4ceb92
Dave Wilson <davidwil@posteo.de>
2022-03-30 21:19:42 +0100

    echo: mostly implement -e and -E escape flags

--- src/echo.scm
+++ src/echo.scm
@@ -6,11 +6,37 @@
 (declare (uses cmdline-util))
 
 (define flag-map (list 
-                    (list "-n" #f)))
+                    (list "-n" #f)
+                    (list "-e" #f)
+                    (list "-E" #f)))
                     
 (define has-n-flag? (command-line-has-flag? "-n"))
+(define has-e-flag? (command-line-has-flag? "-e"))
+(define has-E-flag? (command-line-has-flag? "-E"))
 
 (define line-ending (if has-n-flag? "" "\n"))
+(define interpret-escapes? 
+  (cond ((and has-E-flag? (not has-e-flag?)) #f)
+        (else #t)))
+
+;; Missing escape chars:
+;;   \c (stop output at this point)
+;;   \e (escape)
+;;   \0NNN (octal escape)
+;;   \xHH (hex escape)
+(define (replace-escapes string)
+  (if (not interpret-escapes?)
+    string
+    (string-translate* 
+      string 
+      '(("\\\\" . "\\")
+        ("\\a" . "\a")
+        ("\\b" . "\b")
+        ("\\f" . "\f")
+        ("\\n" . "\n")
+        ("\\r" . "\r")
+        ("\\t" . "\t")
+        ("\\v" . "\v")))))
 
 (define (join-together s)
   (define (inner-join s str)
@@ -23,4 +49,4 @@
   (inner-join s ""))
 
 (define echo-words (get-command-line-without-flags flag-map))
-(printf "~A~A" (join-together echo-words) line-ending)
+(printf "~A~A" (replace-escapes (join-together echo-words)) line-ending)
