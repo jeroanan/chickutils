49a8a2ca5d24292a7f4d1b8556ba2c9c867a79db
Dave Wilson <davidwil@posteo.de>
2022-03-31 19:04:25 +0100

    head: reindent

--- src/head.scm
+++ src/head.scm
@@ -6,10 +6,10 @@
 
 ;; For now, this only prints the first five lines of a single file
 (define (head)
-    (let* ([file-name (car (command-line-arguments))]
-           [lines (file->lines file-name)]
-           [first-five (take 5 lines)]
-           [output (string-intersperse first-five "\n")])
-        (print output)))
+  (let* ([file-name (car (command-line-arguments))]
+         [lines (file->lines file-name)]
+         [first-five (take 5 lines)]
+         [output (string-intersperse first-five "\n")])
+  (print output)))
 
 (head)
