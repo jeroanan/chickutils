db2204b3b81a160f245a35f6853aba1dee5cdc56
Dave Wilson <davidwil@posteo.de>
2022-04-02 18:56:34 +0100

    cat: print line-by-line

--- src/cat.scm
+++ src/cat.scm
@@ -5,16 +5,25 @@
 
 (define flag-map (list))
 
-(define (cat files)
+(define (cat-file file-name)
+  (let ([lines (file->lines file-name)])
+    (define (loop ls)
+      (unless (empty? ls)
+        (begin
+          (print (first ls))
+          (loop (rest ls)))))
+    (loop lines)))
+  
+(define (cat-files files)
   (define (main-loop as)
     (unless (= (length as) 0)
       (begin
-        (print (file->string (car as)))
+        (cat-file (car as))
         (main-loop (cdr as)))))
   (main-loop files))
 
 (define files (get-command-line-without-flags flag-map))
 
 (maybe-print-version-and-quit version-string)
-(cat files)
+(cat-files files)
 
