a62e79def9a76e634137153ccbccd6cff679489c
Dave Wilson <davidwil@posteo.de>
2022-04-02 22:16:43 +0100

    cat: Add -b flag

--- src/cat.scm
+++ src/cat.scm
@@ -3,28 +3,36 @@
 (declare (uses file-util
                cmdline-util))
 
-(define version-string "2022.04.02.02")
+(define version-string "2022.04.02.03")
 
 (define flag-map (list
+                    (list "-b" #f)
                     (list "-n" #f)))
 
+(define has-b-flag? (command-line-has-flag? "-b"))
 (define has-n-flag? (command-line-has-flag? "-n"))
 
+(define doing-numbering? (or has-b-flag? has-n-flag?))
+
+(define (should-number-this-line? line)
+  (and doing-numbering? (not (and (string=? line "") has-b-flag?))))
+
 (define (format-with-line-number line line-number)
-  (if has-n-flag?
-      (let* ([line-number-length (string-length (number->string line-number))]
-             [spaces (make-string (- 6 line-number-length) #\space)])
-        (sprintf "~A~A ~A" spaces line-number line))
-      line))
+  (let* ([line-number-length (string-length (number->string line-number))]
+          [spaces (make-string (- 6 line-number-length) #\space)])
+    (sprintf "~A~A  ~A" spaces line-number line)))
 
 (define (cat-file file-name)
   (let ([lines (file->lines file-name)]
         [line-number 1])
     (define (loop ls)
-      (unless (empty? ls)
-        (begin
-          (print (format-with-line-number (first ls) line-number))
-          (set! line-number (+ line-number 1))
+      (unless (empty? ls) 
+        (let ([line (first ls)])
+          (if (should-number-this-line? line)
+            (begin
+              (print (format-with-line-number line line-number))
+              (set! line-number (+ line-number 1)))
+            (print line))
           (loop (rest ls)))))
     (loop lines)))
   
@@ -40,4 +48,3 @@
 
 (maybe-print-version-and-quit version-string)
 (cat-files files)
-
