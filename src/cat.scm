6309f25fcbdd8d265c31982e498e21671ee1880d
Dave Wilson <davidwil@posteo.de>
2022-04-02 21:49:30 +0100

    cat: Add -n flag

--- src/cat.scm
+++ src/cat.scm
@@ -1,16 +1,30 @@
+(import (chicken format))
+
 (declare (uses file-util
                cmdline-util))
 
-(define version-string "2022.04.02.01")
+(define version-string "2022.04.02.02")
 
-(define flag-map (list))
+(define flag-map (list
+                    (list "-n" #f)))
+
+(define has-n-flag? (command-line-has-flag? "-n"))
+
+(define (format-with-line-number line line-number)
+  (if has-n-flag?
+      (let* ([line-number-length (string-length (number->string line-number))]
+             [spaces (make-string (- 6 line-number-length) #\space)])
+        (sprintf "~A~A ~A" spaces line-number line))
+      line))
 
 (define (cat-file file-name)
-  (let ([lines (file->lines file-name)])
+  (let ([lines (file->lines file-name)]
+        [line-number 1])
     (define (loop ls)
       (unless (empty? ls)
         (begin
-          (print (first ls))
+          (print (format-with-line-number (first ls) line-number))
+          (set! line-number (+ line-number 1))
           (loop (rest ls)))))
     (loop lines)))
   
