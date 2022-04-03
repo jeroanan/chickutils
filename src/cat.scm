691fb9fcff6f2e54f8d7eeb321109a662182089e
Dave Wilson <davidwil@posteo.de>
2022-04-03 10:50:33 +0100

    cat: add s flag

--- src/cat.scm
+++ src/cat.scm
@@ -3,37 +3,51 @@
 (declare (uses file-util
                cmdline-util))
 
-(define version-string "2022.04.03.01")
+(define version-string "2022.04.03.02")
 
 (define flag-map (list
                     (list "-b" #f)
                     (list "-n" #f)
+                    (list "-s" #f)
                     (list "-u" #f)))
 
 (define has-b-flag? (command-line-has-flag? "-b"))
 (define has-n-flag? (command-line-has-flag? "-n"))
+(define has-s-flag? (command-line-has-flag? "-s"))
 
 (define doing-numbering? (or has-b-flag? has-n-flag?))
 
-(define (should-number-this-line? line)
-  (and doing-numbering? (not (and (string=? line "") has-b-flag?))))
+(define (suppress-line? line-text last-line-blank?)
+  (let ([line-empty? (string=? line-text "")])
+    (and line-empty? last-line-blank? has-s-flag?)))
 
-(define (format-with-line-number line line-number)
-  (let* ([line-number-length (string-length (number->string line-number))]
+(define (should-number-this-line? line last-line-blank?)
+  (and doing-numbering? 
+       (not (and (string=? line "") has-b-flag?))
+       (not (suppress-line? line last-line-blank?))))
+
+(define (format-with-line-number line line-number last-line-blank?)
+  (if (should-number-this-line? line last-line-blank?)
+    (let* ([line-number-length (string-length (number->string line-number))]
           [spaces (make-string (- 6 line-number-length) #\space)])
-    (sprintf "~A~A  ~A" spaces line-number line)))
+      (sprintf "~A~A  ~A" spaces line-number line))
+    line))
+
+(define (print-line line-text line-number last-line-blank?)
+  (unless (suppress-line? line-text last-line-blank?)
+    (print (format-with-line-number line-text line-number last-line-blank?))))
 
 (define (cat-file file-name)
   (let ([lines (file->lines file-name)]
-        [line-number 1])
+        [line-number 1]
+        [last-line-blank? #f])
     (define (loop ls)
       (unless (empty? ls) 
         (let ([line (first ls)])
-          (if (should-number-this-line? line)
-            (begin
-              (print (format-with-line-number line line-number))
+          (print-line line line-number last-line-blank?)
+          (when (should-number-this-line? line last-line-blank?)
               (set! line-number (+ line-number 1)))
-            (print line))
+          (set! last-line-blank? (string=? line ""))
           (loop (rest ls)))))
     (loop lines)))
   
